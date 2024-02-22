5298bd55a9047ad5157ca624059a060e96655dd3
Vanilo Ferreira <vanilo.ferreira@fdte.io>
2024-02-22 00:53:52 -0300

    feat: Adicionar função para Rotas protegidas após o login

--- src/routes.js
+++ src/routes.js
@@ -1,24 +1,49 @@
-import React from 'react';
+import React, { createContext, useState, useContext } from 'react';
 import {
   BrowserRouter,
-  Routes,
-  Route
+  Switch,
+  Route,
+  Redirect
 } from "react-router-dom";
 
 import Cadastro from './pages/Cadastro';
 import Login from './pages/Login';
 import Holdings from './pages/Holdings';
 
-function Rotas() {
+export const AuthContext = createContext();
+
+function ProtectedRoutes(props) {
+  const { token } = useContext(AuthContext);
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/cadastro" element={<Cadastro />} />
-        <Route path="/" exact element={<Login />} />
-        <Route path="/holdings" exact element={<Holdings />} />
-      </Routes>
-    </BrowserRouter>
+    <Route
+      render={() => (token ? props.children : <Redirect to="/" />)} />
+  );
+}
+
+function Rotas() {
+  const [token, setToken] = useState('');
+
+  function logar(token) {
+    setToken(token);
+  }
+
+  function deslogar() {
+    setToken('')
+  }
+
+  return (
+    <AuthContext.Provider value={{ token, logar, deslogar }}>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/cadastro" component={Cadastro} />
+          <Route path="/" exact component={Login} />
+          <ProtectedRoutes>
+            <Route path="/holdings" exact component={Holdings} />
+          </ProtectedRoutes>
+        </Switch>
+      </BrowserRouter>
+    </AuthContext.Provider>
   )
 }
 
